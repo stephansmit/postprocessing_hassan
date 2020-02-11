462f35fbea7f1e9fc9fc868b4f36e924acb7623e
stephan <stephansmit@hotmail.com>
2020-02-11 17:33:08 +0100

    added th emain

--- main.f90
+++ main.f90
@@ -2,19 +2,24 @@ program post_hassan
   implicit none
   include 'param.txt'
   include 'common.txt'
-  integer counter_post, f
+  integer counter_post, nfiles,f
   !make the grid
+
+  nfiles = 1
+
+
   call mkgrid(0)
 
    
 
   OPEN(101, file= 'output.dat', status='replace')
   !load the data
-  do f=0,95
+  do f=0,nfiles-1
      call load_avg_post(0,counter_post, f)
+     call load_avg(f)
      do k =1,kmax
        do i= 1,imax
-         write(101,"(3E20.10)") k*dz+kmax*f*dz,rp(i),upp(i,k),vpp(i,k), wpp(i,k),RUWpp(i,k)
+         write(101,"(7E20.10)") k*dz+kmax*f*dz,rp(i),upp(i,k),vpp(i,k), wpp(i,k),RUWpp(i,k),Rnnff(i,k,1) 
        enddo
      enddo
   enddo
